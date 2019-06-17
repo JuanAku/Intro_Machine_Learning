90fce65575998bdb63b12ea3e04a3eb6f821702e
JuanAku <31018092+JuanAku@users.noreply.github.com>
2019-06-17 10:26:48 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,2 +1,14 @@
-import numpy
+import numpy as np
 print("hola mundo")
+
+from matplotlib import pyplot as plt
+
+A = [1,2,3]
+B = [4,5,6]
+
+A = np.array(A)
+B = np.array(B)
+
+plt.figure()
+plt.scatter(A,B)
+plt.show()
